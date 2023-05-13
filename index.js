bd2f861477af50da813e078c032ec6d6058592f7
Vishnu Vardhan <125671722+VishnuVardhanBonala-web4U@users.noreply.github.com>
2023-05-13 09:53:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function showprofile(){
+ const profile=document.querySelector("#profilebody")
+ profile.classList.toggle("displayprofile")
+}
+
+function btnmove(){
+   const btnmove=document.querySelector("#themebtn")
+    btnmove.classList.toggle('toggle-btnmove')
+
+}
